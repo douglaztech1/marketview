38ca765a0ce4101bfbdedc7c80ac35adac6f5873
Douglas <douglaztech1@gmail.com>
2026-06-25 16:09:48 -0300

    Protege dados de conexão

--- backend/conexao.py
+++ backend/conexao.py
@@ -1,20 +1,19 @@
+import os
 import psycopg2
 
-def conectar():
 
+def conectar():
     try:
         conexao = psycopg2.connect(
-            host="ep-royal-thunder-acob01h8.sa-east-1.aws.neon.tech",
-            database="neondb",
-            user="neondb_owner",
-            password="npg_RECMjH6qX2Ik",
+            host=os.getenv("DB_HOST"),
+            database=os.getenv("DB_NAME"),
+            user=os.getenv("DB_USER"),
+            password=os.getenv("DB_PASSWORD"),
             sslmode="require"
         )
 
-        print("Conexão realizada com sucesso!")
         return conexao
 
     except Exception as erro:
-        print("Erro ao conectar:")
         print(erro)
         return None
